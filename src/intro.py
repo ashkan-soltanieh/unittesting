a831466287d6bf6316a3b5cc30d70451dfa3cbca
Ashkan Soltanieh <ashkan98@gmail.com>
2025-01-05 18:24:09 -0700

    Implement test cases for get_coupons

--- src/intro.py
+++ src/intro.py
@@ -1,2 +1,6 @@
 def find_max(a, b):
     return a if a > b else b
+
+
+def get_coupons():
+    return [{"code": "SAVE20", "discount": 0.2}, {"code": "SAVE50", "discount": 0.5}]
--- tests/test_intro.py
+++ tests/test_intro.py
@@ -1,4 +1,4 @@
-from src.intro import find_max
+from src.intro import find_max, get_coupons
 
 
 def test_find_max_should_return_the_first_argument_if_it_is_greater():
@@ -19,3 +19,20 @@ def test_find_max_should_return_the_second_argument_if_it_is_greater():
 
 def test_find_max_should_return_the_first_argument_if_arguments_are_equal():
     assert find_max(1, 1) == 1
+
+
+def test_get_coupons_returns_array_with_items():
+    assert len(get_coupons()) > 0
+
+
+def test_get_coupons_contain_code_and_discount_as_keys():
+    assert "code" in get_coupons()[0].keys()
+    assert "discount" in get_coupons()[0].keys()
+
+
+def test_get_coupons_code_is_string_type():
+    assert isinstance(get_coupons()[0]["code"], str)
+
+
+def test_get_coupons_discount_is_float_type():
+    assert isinstance(get_coupons()[0]["discount"], float)
