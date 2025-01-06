5290dbe6508d4ac1e13c1e67572280049240aa8b
Ashkan Soltanieh <ashkan98@gmail.com>
2025-01-05 19:56:29 -0700

    Add positive and negative tests

--- src/intro.py
+++ src/intro.py
@@ -4,3 +4,16 @@ def find_max(a, b):
 
 def get_coupons():
     return [{"code": "SAVE20", "discount": 0.2}, {"code": "SAVE50", "discount": 0.5}]
+
+
+def calculate_discount(code: str, price: int) -> float:
+    if type(price) is not int or price <= 0:
+        raise ValueError("Price must be a positive integer type")
+
+    coupon = [coupon for coupon in get_coupons() if coupon["code"] == code]
+
+    if not coupon:
+        return price
+
+    discount = coupon[0]["discount"]
+    return price - discount * price
--- tests/test_intro.py
+++ tests/test_intro.py
@@ -1,4 +1,5 @@
-from src.intro import find_max, get_coupons
+from src.intro import find_max, get_coupons, calculate_discount
+import pytest
 
 
 def test_find_max_should_return_the_first_argument_if_it_is_greater():
@@ -36,3 +37,23 @@ def test_get_coupons_code_is_string_type():
 
 def test_get_coupons_discount_is_float_type():
     assert isinstance(get_coupons()[0]["discount"], float)
+
+
+def test_calculate_discount_returns_discounted_price_if_given_code_is_valid():
+    result = calculate_discount("SAVE20", 10)
+    assert result == 8
+
+
+def test_calculate_discount_handles_non_numeric_price():
+    with pytest.raises(ValueError):
+        calculate_discount("SAVE20", "10")
+
+
+def test_calculate_discount_handles_negative_price():
+    with pytest.raises(ValueError):
+        calculate_discount("SAVE20", -10)
+
+
+def test_calculate_discount_returns_price_itself_when_code_is_invalid():
+    result = calculate_discount("Invalid_Code", 10)
+    assert result == 10
